1013719802048e78da49fbc7696e8b8c4795f75a
Tauan Benevides <tg.dossantosbenevides@gmail.com>
2024-08-01 13:03:55 -0300

    feat: adiciona o script que faz tocar a musica tema do projeto ao clicar no personagem

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// Musica abertura ao cliccar
+
+function playAudio(){
+    play()
+  }
+ function play() {
+   var audio = document.getElementById("audio");
+   audio.play();
+ }
+
+ // Logica de progamaçao
